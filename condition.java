fc9e5b730eaffa70ad9517b043be499c49e78b39
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-02-15 11:59:22 +0530

    added java practice consitions

---
+++ condition.java
@@ -0,0 +1,39 @@
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+       int x = 10;
+       int y = 30;
+       if(x != 10){
+           System.out.println("yes");
+       }
+       else if(x > 5){
+           System.out.println("yes yes");
+       }
+       else{
+           System.out.println("No");
+       }
+    }
+}
+
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+       int x = 10;
+       int y = 10;
+       if(x > y && y > x){
+           System.out.println("yes");
+       }
+       else if(x < y || y < x){
+           System.out.println("yes yes");
+       }
+       else{
+           System.out.println("No");
+       }
+    }
+}
---
+++ operator.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        double ans = 0;
+        int ans1 = 0;
+        ans1 = a+b;
+        ans = a-b;
+        ans = a/b;
+        ans = a%b;
+        System.out.println(ans1);
+        System.out.println(ans);
+
+    }
+}
